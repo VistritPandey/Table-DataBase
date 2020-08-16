793127160980517ff6ac103d840c2f1cd52d7d0c
VistritPandey <56611775+VistritPandey@users.noreply.github.com>
2020-08-16 11:02:52 +0530

    Add files via upload

---
+++ js/module.js
@@ -0,0 +1,71 @@
+const productsdb = (dbname, table) => {
+  const db = new Dexie(dbname);
+  db.version(1).stores(table);
+  db.open();
+
+  return db;
+ 
+};
+
+const bulkcreate = (dbtable, data) => {
+  let flag = empty(data);
+  if (flag) {
+    dbtable.bulkAdd([data]);
+    console.log("data inserted successfully...!");
+  } else {
+    console.log("Please provide data...!");
+  }
+  return flag;
+};
+
+const createEle = (tagname, appendTo, fn) => {
+  const element = document.createElement(tagname);
+  if (appendTo) appendTo.appendChild(element);
+  if (fn) fn(element);
+};
+
+const empty = object => {
+  let flag = false;
+  for (const value in object) {
+    if (object[value] != "" && object.hasOwnProperty(value)) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+  }
+  return flag;
+};
+
+const getData = (dbname, fn) => {
+  let index = 0;
+  let obj = {};
+  dbname.count(count => {
+    if (count) {
+      dbname.each(table => {
+        obj = SortObj(table);
+        fn(obj, index++); 
+      });
+    } else {
+      fn(0);
+    }
+  });
+};
+
+const SortObj = (sortobj) => {
+  let obj = {};
+  obj = {
+    id: sortobj.id,
+    name: sortobj.name,
+    quantity: sortobj.quantity,
+    price: sortobj.price
+  };
+  return obj;
+}
+
+export default productsdb;
+export {
+  bulkcreate,
+  createEle,
+  getData,
+  SortObj
+};
